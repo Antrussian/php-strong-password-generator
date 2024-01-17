6f095dc2260836e5bca5ba0a8b3f904e8fe0e7c3
Antrussian <antonino.russo.agent@gmail.com>
2024-01-18 00:57:49 +0100

    Inserita la logica di pass random alfanumerica

--- index.php
+++ index.php
@@ -19,11 +19,17 @@
 </form>
 
 <?php
-
 $passInput = $_GET['passInput'] ?? '0';
-echo $passInput;
 
+if ($passInput == 0) {
+    echo 'Inserisci un numero';
+} else {
+    $bytes = openssl_random_pseudo_bytes($passInput);
+    $pass = bin2hex($bytes);
+    echo $pass;
+}
 ?>
+
     
 </body>
 </html>
